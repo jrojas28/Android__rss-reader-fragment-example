20d55c95d216dc885398c9e97adef1a385f60540
Jaime Rojas <j_rojas94@hotmail.com>
2018-05-12 20:42:37 -0400

    Added comments to RSS Adapter Class

--- app/src/main/java/com/altice_crt_b/rssreader/Adapters/RSSAdapter.java
+++ app/src/main/java/com/altice_crt_b/rssreader/Adapters/RSSAdapter.java
@@ -22,6 +22,12 @@ import java.util.zip.Inflater;
  */
 
 public class RSSAdapter extends ArrayAdapter {
+    /**
+     * ----------------------------
+     * ------ Class Variables------
+     * ----------------------------
+     * ArrayList<article> articles      The article objects held by the adapter.
+     */
     public ArrayList<Article> articles;
 
     public RSSAdapter(@NonNull Context context, int resource) {
@@ -44,13 +50,26 @@ public class RSSAdapter extends ArrayAdapter {
         return this.articles.indexOf(object);
     }
 
+    /**
+     * setArticles      a setter intended to change the article list as a whole.
+     * @param articles  The article list that'll be used by the Adapter.
+     */
     public void setArticles(ArrayList<Article> articles) {
         this.articles = articles;
+        //Because we need to update the view too, we notify changes
+        //This allows further events to know of the new article array and
+        //proceed updating the view as expected.
         notifyDataSetChanged();
-
-        Log.wtf("RSS_ADAPTER", "Data Set Changed");
     }
 
+    /**
+     * getCount         a function intended to obtain the amount of elements in the objects list.
+     *                  Due to how we send initially an empty object array to the SUPER on the
+     *                  constructor, we need to override this function to use our own articles list
+     *                  which can be overriden by the setArticles function. Otherwise, the count will
+     *                  only return 0 because no list is passed to SUPER in the first constructor.
+     * @return count    The amount of elements within the articles list.
+     */
     @Override
     public int getCount() {
         return this.articles.size();
@@ -60,13 +79,17 @@ public class RSSAdapter extends ArrayAdapter {
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         if(convertView == null) {
+            //If there's no view, that means this article is a new one not yet processed.
+            Article article = this.articles.get(position);
+            //Now, we'll need to inflate an Article Container layout, providing us with a full View
+            //with the fields necessary to fill in the data for the new article.
             convertView = (View) LayoutInflater.from(this.getContext()).inflate(R.layout.article_container, parent, false);
-            ((TextView) convertView.findViewById(R.id.title)).setText(this.articles.get(position).getTitle());
-            ((TextView) convertView.findViewById(R.id.author)).setText(this.articles.get(position).getAuthor());
-            ((TextView) convertView.findViewById(R.id.description)).setText(this.articles.get(position).getDescription());
-            ((TextView) convertView.findViewById(R.id.pubDate)).setText(this.articles.get(position).getPubDate().toString());
+            ((TextView) convertView.findViewById(R.id.title)).setText(article.getTitle());
+            ((TextView) convertView.findViewById(R.id.author)).setText(article.getAuthor());
+            ((TextView) convertView.findViewById(R.id.description)).setText(article.getDescription());
+            ((TextView) convertView.findViewById(R.id.pubDate)).setText(article.getPubDate().toString());
         }
-        Log.wtf("RSS_ADAPTER", "Creating Object " + position);
+        //Finally, return the view.
         return convertView;
     }
 
